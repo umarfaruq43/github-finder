bcae5746c6c992b9b9bff49bbfed2cbe5eb1e466
Umarfaruq43 <abdulazeezfaruq43@gmail.com>
2021-10-25 22:20:01 +0100

    Add files

--- src/components/cards/Links.js
+++ src/components/cards/Links.js
@@ -3,7 +3,7 @@ import { FaBuilding, FaLink, FaMapMarkerAlt, FaTwitter } from "react-icons/fa";
 import { BrowserRouter, NavLink } from "react-router-dom";
 
 const Links = ({ data }) => {
-  const { location, twitter_username, html_url } = data;
+  const { location, twitter_username, html_url, type } = data;
   return (
     <BrowserRouter>
       <div className="equal_div mb-3 ">
@@ -16,9 +16,9 @@ const Links = ({ data }) => {
 
         <div className="w-1/2 equal_div">
           <FaTwitter />
-          <NavLink to="link" className="inline-block ml-4 text-sm">
+          <span className="inline-block ml-4 text-sm">
             {twitter_username ? twitter_username : "Not Specified"}
-          </NavLink>
+          </span>
         </div>
       </div>
       <div className="equal_div ">
@@ -28,13 +28,15 @@ const Links = ({ data }) => {
             to={html_url ? html_url : "#"}
             className="inline-block ml-4 text-sm"
           >
-            {html_url ? "Vist site" : " Not Specified "}
+            {html_url ? "Go to gitHub" : " Not Specified "}
           </NavLink>
         </div>
 
         <div className="w-1/2 equal_div">
           <FaBuilding />
-          <span className="inline-block ml-4 text-sm">San Francisco</span>
+          <span className="inline-block ml-4 text-sm">
+            {type ? type : " Not specified "}
+          </span>
         </div>
       </div>
     </BrowserRouter>
